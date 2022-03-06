4e05460b86b9ac1ecf4c14bbe08b348bdb770092
Thalerngsak Patchakhiew <oodramcom@gmail.com>
2022-03-06 13:39:08 +0700

    Add Spring Boot

---
+++ src/test/java/com/thalerngsak/assignment/java/boot/camp/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thalerngsak.assignment.java.boot.camp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
